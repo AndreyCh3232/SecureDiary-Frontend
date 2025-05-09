49b530cae99936d346e7f73696bc0c3464184107
Andrey Chetvergov <andrey32102@gmail.com>
2025-05-09 12:06:31 +0300

    Fix the code

--- src/index.jsx
+++ src/index.jsx
@@ -6,18 +6,17 @@ import { Provider } from 'react-redux'
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-import { store } from './store/store'
+// import { store } from './store/store'
 import { RootCmp } from './RootCmp'
 
 import './assets/styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<Provider store={store}>
-		<Router>
-			<RootCmp />
-		</Router>
-	</Provider>
+
+	<Router>
+		<RootCmp />
+	</Router>
 )
 
 // If you want your app to work offline and load faster, you can change

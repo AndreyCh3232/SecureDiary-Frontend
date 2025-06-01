ca00934b537312b937b0d03a0eabff7550f016a7
Andrey Chetvergov <andrey32102@gmail.com>
2025-06-01 16:07:06 +0300

    Fix base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-	base: '/SecureDiary/',
+	base: '/SecureDiary-Frontend/',
 	plugins: [react()],
 })
 

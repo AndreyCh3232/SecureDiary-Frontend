030ce14f4ac83cfc6cb8bbab0654656bc2e582ec
Andrey Chetvergov <andrey32102@gmail.com>
2025-06-01 15:48:19 +0300

    Fix the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const BASE_URL = '/SecureDiary/'
+// const BASE_URL = '/SecureDiary/'
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	base: BASE_URL,
+// })
 
 export default defineConfig({
+	base: '/SecureDiary/',
 	plugins: [react()],
-	base: BASE_URL,
 })
 
 // export default defineConfig({

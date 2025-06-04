ade885763ffa310918c61759af5b95ca4783f905
Andrey Chetvergov <andrey32102@gmail.com>
2025-06-04 16:12:15 +0300

    Fix the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-	base: '/SecureDiary-Frontend/',
 	plugins: [react()],
+	build: {
+		outDir: '../SecureDiary-Backend/public',
+		emptyOutDir: true,
+	},
 })
